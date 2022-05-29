e29003585922db7c6647ed2af1a73191ce897961
KarolDawidG <karoldawidg@gmail.com>
2022-05-29 18:15:39 +0200

    Log 2

--- unit1.pas
+++ unit1.pas
@@ -12,13 +12,18 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    Edit1: TEdit;
     Losuj: TButton;
     Sortuj: TButton;
     Info: TButton;
     Koniec: TButton;
     LosowaLista: TListBox;
     SortowanaLista: TListBox;
-    procedure LosowaListaClick(Sender: TObject);
+    procedure InfoClick(Sender: TObject);
+    procedure KoniecClick(Sender: TObject);
+    procedure LosujClick(Sender: TObject);
+
+
   private
 
   public
@@ -33,10 +38,24 @@ implementation
 {$R *.lfm}
 
 { TForm1 }
+  var liczba1, liczba2, suma : integer;
 
-procedure TForm1.LosowaListaClick(Sender: TObject);
+procedure TForm1.LosujClick(Sender: TObject);
 begin
+  liczba1 :=10;
+  liczba2 :=20;
+  suma :=  liczba1 + liczba2;
+  Edit1.Text := FloatToStr(suma);
+end;
 
+procedure TForm1.InfoClick(Sender: TObject);
+begin
+  ShowMessage('Autor programu: Dawid Glinkowski');
+end;
+
+procedure TForm1.KoniecClick(Sender: TObject);
+begin
+    ShowMessage('Nie dziala');
 end;
 
 end.
