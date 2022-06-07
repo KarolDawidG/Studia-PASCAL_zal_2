b194c061563086a74f26ef4aa8424a0a62bf634f
KarolDawidG <karoldawidg@gmail.com>
2022-06-07 14:04:06 +0200

    Log 5

--- unit1.pas
+++ unit1.pas
@@ -8,6 +8,24 @@ uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+  PLista = ^Tlista;
+  Tlista =  record
+    next  : PLista;
+    data  : integer;
+  end;
+
+// Definicja typu obiektowego slist
+  slist = object
+    public
+      head : PLista;  // początek listy
+      constructor init;
+      destructor destroy;
+      function size : cardinal;
+      procedure printl;
+      procedure push_front ( v : longint );
+  end;
+
+type
 
   { TForm1 }
 
@@ -30,37 +48,75 @@ type
 
 var
   Form1: TForm1;
-  arrString : array[1..6] of integer;
+
 
 implementation
 
+ constructor slist.init;
+begin
+  head := nil;
+end;
+
+destructor slist.destroy;
+begin
+end;
+
+// Funkcja oblicza liczbę elementów listy
+function slist.size : cardinal;
+var
+  c : cardinal;
+  p : PLista;
+begin
+  c := 0;
+  p := head;
+  while p <> nil do
+  begin
+    inc ( c );
+    p := p^.next;
+  end;
+  size := c;
+end;
+
+// Procedura wyświetla zawartość elementów listy
+procedure slist.printl;
+var
+  i : integer;
+  p : PLista;
+begin
+  Randomize;
+  p := head;
+  i := 1;
+  while p <> nil do
+  begin
+    writeln ( 'Nr: ', i, '  liczba = ', Random (200) );
+    inc ( i );
+    p := p^.next;
+  end;
+  writeln;
+end;
+
+// Procedura dołączania na początek listy
+procedure slist.push_front ( v : longint );
+var
+  p : PLista;
+begin
+  new ( p );
+  p^.next := head;
+  p^.data := v;
+  head    := p;
+end;
 {$R *.lfm}
 
 { TForm1 }
-  var liczba1, liczba2, suma, suma2: integer;
+ // var liczba1 : integer;   jakies zmienne globalne.
 
 procedure TForm1.LosujClick(Sender: TObject);
-var
-sFor1:integer;
 begin
-Randomize;
-     arrString[1] := random(100);
-     arrString[2] := random(100);
-     arrString[3] := random(100);
-     arrString[4] := random(100);
-     arrString[5] := random(100);
-     arrString[6] := random(100);
-    For sFor1 in arrString do
-    LosowaLista.Items.Add(IntToStr(sFor1));
+
 end;
 
-
-
-
 procedure TForm1.SortujClick(Sender: TObject);
-var i : integer;
 begin
-
 
 end;
 
